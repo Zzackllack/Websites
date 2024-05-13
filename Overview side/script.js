78cb6cd6ab893414f74fbc0a86205806d035f2b8
Zzacklack <149129501+Zzackllack@users.noreply.github.com>
2024-05-13 15:35:48 +0200

    Uploaded my Overview side
    It includes a responsive layout that adapts to various screen sizes and devices. Smooth animations and transitions for a visually appealing user experience. Custom pop-ups and alerts that provide information without redirection. Integration of social media links with dynamic effects.

---
+++ Overview side/script.js
@@ -0,0 +1,30 @@
+/** @format */
+
+document.getElementById("profile-picture").onclick = function () {
+  var overlay = document.getElementById("image-overlay");
+  var overlayImg = overlay.querySelector(".overlay-profilbild");
+  overlay.classList.add("active");
+  overlayImg.style.width = "300px"; // Vergrößert das Bild auf 300px
+};
+
+function closeOverlay() {
+  var overlay = document.getElementById("image-overlay");
+  var overlayImg = overlay.querySelector(".overlay-profilbild");
+  overlay.classList.remove("active");
+  overlayImg.style.width = "120px"; // Setzt die Bildgröße auf die Anfangsgröße zurück
+}
+
+
+function openPopup() {
+  document.getElementById("discordPopup").style.display = "flex";
+  setTimeout(() => {
+    document.getElementById("discordPopup").classList.add("active");
+  }, 10); // Delay hinzufügen, damit die CSS-Animation wirken kann
+}
+
+function closePopup() {
+  document.getElementById("discordPopup").classList.remove("active");
+  setTimeout(() => {
+    document.getElementById("discordPopup").style.display = "none";
+  }, 300); // Warte bis die Animation beendet ist
+}
