fe58f1117e4083e35cdb52805d3f9274faebb8d5
zzacklack <zzacklack@gmail.com>
2024-09-26 21:59:57 +0000

    Added comment

--- Login/check_session.php
+++ Login/check_session.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
 
+// Überprüfung, ob der Benutzer eingeloggt ist
 if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
-    header('Location: login.php'); // Umleitung zur Login-Seite
+    header('Location: /login.php'); // Umleitung zur Login-Seite
     exit;
 }
 ?>
