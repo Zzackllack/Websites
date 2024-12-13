25bae6a9bd941452da16fa50a6db222ddc689075
Zacklack <contact@zacklack.de>
2024-12-13 22:57:20 +0100

    Added Umami analytics script to the login page for performance tracking

--- Login/login.php
+++ Login/login.php
@@ -31,6 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Zacklack</title>
+    <script defer src="https://cloud.umami.is/script.js" data-website-id="6ac3974f-c908-4f8c-a726-ef8845e80677"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap" rel="stylesheet">
