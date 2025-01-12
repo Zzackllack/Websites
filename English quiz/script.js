73898623f8b1486294bd4cb1cdbc9ee249a5c8c0
Zacklack <contact@zacklack.de>
2025-01-12 19:24:29 +0100

    Split of one .html file into .js and .css files

---
+++ English quiz/script.js
@@ -0,0 +1,417 @@
+    var correctAnswers1 = ["some"];
+    
+        function checkAnswer1() {
+        var userAnswer = document.getElementById('user-answer1').value;
+        var resultElement = document.getElementById('result1');
+        if(correctAnswers1.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+    
+        var correctAnswers2 = ["some"];
+    
+        function checkAnswer2() {
+        var userAnswer = document.getElementById('user-answer2').value;
+        var resultElement = document.getElementById('result2');
+        if(correctAnswers2.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+            
+        var correctAnswers3 = ["any"];
+    
+        function checkAnswer3() {
+        var userAnswer = document.getElementById('user-answer3').value;
+        var resultElement = document.getElementById('result3');
+        if(correctAnswers3.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+            
+        var correctAnswers4 = ["any"];
+    
+        function checkAnswer4() {
+        var userAnswer = document.getElementById('user-answer4').value;
+        var resultElement = document.getElementById('result4');
+        if(correctAnswers4.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+            
+        var correctAnswers5 = ["some"];
+    
+        function checkAnswer5() {
+        var userAnswer = document.getElementById('user-answer5').value;
+        var resultElement = document.getElementById('result5');
+        if(correctAnswers5.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+            
+        var correctAnswers6 = ["any"];
+    
+        function checkAnswer6() {
+        var userAnswer = document.getElementById('user-answer6').value;
+        var resultElement = document.getElementById('result6');
+        if(correctAnswers6.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+            
+        var correctAnswers7 = ["some"];
+    
+        function checkAnswer7() {
+        var userAnswer = document.getElementById('user-answer7').value;
+        var resultElement = document.getElementById('result7');
+        if(correctAnswers7.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+            
+        var correctAnswers8 = ["much"];
+    
+        function checkAnswer8() {
+        var userAnswer = document.getElementById('user-answer8').value;
+        var resultElement = document.getElementById('result8');
+        if(correctAnswers8.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+            
+        var correctAnswers9 = ["many","lots of","a lot of"];
+    
+        function checkAnswer9() {
+        var userAnswer = document.getElementById('user-answer9').value;
+        var resultElement = document.getElementById('result9');
+        if(correctAnswers9.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+
+        var correctAnswers10 = ["many","lots of","a lot of"];
+    
+        function checkAnswer10() {
+        var userAnswer = document.getElementById('user-answer10').value;
+        var resultElement = document.getElementById('result10');
+        if(correctAnswers10.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+        var correctAnswers11 = ["much"];
+    
+        function checkAnswer11() {
+        var userAnswer = document.getElementById('user-answer11').value;
+        var resultElement = document.getElementById('result11');
+        if(correctAnswers11.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+        var correctAnswers12 = ["lots of","a lot of"];
+    
+        function checkAnswer12() {
+        var userAnswer = document.getElementById('user-answer12').value;
+        var resultElement = document.getElementById('result12');
+        if(correctAnswers12.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+        var correctAnswers13 = ["many","lots of","a lot of"];
+    
+        function checkAnswer13() {
+        var userAnswer = document.getElementById('user-answer13').value;
+        var resultElement = document.getElementById('result13');
+        if(correctAnswers13.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+        var correctAnswers14 = ["many"];
+    
+        function checkAnswer14() {
+        var userAnswer = document.getElementById('user-answer14').value;
+        var resultElement = document.getElementById('result14');
+        if(correctAnswers14.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+        var correctAnswers15 = ["many","lots of","a lot of"];
+    
+        function checkAnswer15() {
+        var userAnswer = document.getElementById('user-answer15').value;
+        var resultElement = document.getElementById('result15');
+        if(correctAnswers15.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+        var correctAnswers16 = ["many"];
+    
+        function checkAnswer16() {
+        var userAnswer = document.getElementById('user-answer16').value;
+        var resultElement = document.getElementById('result16');
+        if(correctAnswers16.includes(userAnswer)) {
+            resultElement.innerText = "Correct!";
+            resultElement.style.color = "green"; // Add this line
+        } else {
+            resultElement.innerText = "Incorrect";
+            resultElement.style.color = "red"; // Add this line
+        }
+    }
+        var isSentenceCorrect17 = true; // Change this to the correct answer
+
+        function checkAnswer17(userAnswer) {
+            var resultElement = document.getElementById('result17');
+            if(userAnswer === isSentenceCorrect17) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                resultElement.innerText = "Incorrect";
+                resultElement.style.color = "red";
+            }
+        }
+        var isSentenceCorrect18 = false; // Change this to the correct answer
+
+        function checkAnswer18(userAnswer) {
+            var resultElement = document.getElementById('result18');
+            if(userAnswer === isSentenceCorrect18) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                resultElement.innerText = "Incorrect";
+                resultElement.style.color = "red";
+            }
+        }
+        var isSentenceCorrect19 = false; // Change this to the correct answer
+
+        function checkAnswer19(userAnswer) {
+            var resultElement = document.getElementById('result19');
+            if(userAnswer === isSentenceCorrect19) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                resultElement.innerText = "Incorrect";
+                resultElement.style.color = "red";
+            }
+        }
+        var isSentenceCorrect20 = true; // Change this to the correct answer
+
+        function checkAnswer20(userAnswer) {
+            var resultElement = document.getElementById('result20');
+            if(userAnswer === isSentenceCorrect20) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                resultElement.innerText = "Incorrect";
+                resultElement.style.color = "red";
+            }
+        }
+        var isSentenceCorrect21 = false; // Change this to the correct answer
+
+        function checkAnswer21(userAnswer) {
+            var resultElement = document.getElementById('result21');
+            if(userAnswer === isSentenceCorrect21) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                resultElement.innerText = "Incorrect";
+                resultElement.style.color = "red";
+            }
+        }
+        var correctAnswer22 = "I have some apples in the kitchen.";
+        var attempts22 = 0;
+
+        function checkAnswer22() {
+            var userAnswer = document.getElementById('user-answer22').value;
+            var resultElement = document.getElementById('result22');
+            if(userAnswer === correctAnswer22) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                attempts22++;
+                if(attempts22 >= 3) {
+                    resultElement.innerText = "Incorrect. The correct answer is: " + correctAnswer22;
+                    resultElement.style.color = "red";
+                } else {
+                    resultElement.innerText = "Incorrect. Try again.";
+                    resultElement.style.color = "red";
+                }
+            }
+        }
+        var correctAnswer23 = "Are there any places you would like to visit?";
+        var attempts23 = 0;
+
+        function checkAnswer23() {
+            var userAnswer = document.getElementById('user-answer23').value;
+            var resultElement = document.getElementById('result23');
+            if(userAnswer === correctAnswer23) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                attempts23++;
+                if(attempts23 >= 3) {
+                    resultElement.innerText = "Incorrect. The correct answer is: " + correctAnswer23;
+                    resultElement.style.color = "red";
+                } else {
+                    resultElement.innerText = "Incorrect. Try again.";
+                    resultElement.style.color = "red";
+                }
+            }
+        }
+        var correctAnswer24 = ["He has a lot of friends in the city.", "He has lots of friends in the city.", "He has many friends in the city."];
+        var attempts24 = 0;
+
+        function checkAnswer24() {
+            var userAnswer = document.getElementById('user-answer24').value;
+            var resultElement = document.getElementById('result24');
+            if(correctAnswer24.includes(userAnswer)) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                attempts24++;
+                if(attempts24 >= 3) {
+                    resultElement.innerText = "Incorrect. The correct answer is: " + correctAnswer24;
+                    resultElement.style.color = "red";
+                } else {
+                    resultElement.innerText = "Incorrect. Try again.";
+                    resultElement.style.color = "red";
+                }
+            }
+        }
+        var correctAnswer25 = "There isn´t much time to finish the project.";
+        var attempts25 = 0;
+
+        function checkAnswer25() {
+            var userAnswer = document.getElementById('user-answer25').value;
+            var resultElement = document.getElementById('result25');
+            if(userAnswer === correctAnswer25) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                attempts25++;
+                if(attempts25 >= 3) {
+                    resultElement.innerText = "Incorrect. The correct answer is: " + correctAnswer25;
+                    resultElement.style.color = "red";
+                } else {
+                    resultElement.innerText = "Incorrect. Try again.";
+                    resultElement.style.color = "red";
+                }
+            }
+        }
+        var correctAnswer26 = ["She has read many of books to prepare for the exam.", "She has read lots of books to prepare for the exam.", "She has read a lot of books to prepare for the exam."];
+        var attempts26 = 0;
+
+        function checkAnswer26() {
+            var userAnswer = document.getElementById('user-answer26').value;
+            var resultElement = document.getElementById('result26');
+            if(correctAnswer26.includes(userAnswer)) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                attempts26++;
+                if(attempts26 >= 3) {
+                    resultElement.innerText = "Incorrect. The correct answer is: " + correctAnswer26;
+                    resultElement.style.color = "red";
+                } else {
+                    resultElement.innerText = "Incorrect. Try again.";
+                    resultElement.style.color = "red";
+                }
+            }
+        }
+        var correctAnswer27 = "Do you have any experience with complex software programs?";
+        var attempts27 = 0;
+
+        function checkAnswer27() {
+            var userAnswer = document.getElementById('user-answer27').value;
+            var resultElement = document.getElementById('result27');
+            if(userAnswer === correctAnswer27) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                attempts27++;
+                if(attempts27 >= 3) {
+                    resultElement.innerText = "Incorrect. The correct answer is: " + correctAnswer27;
+                    resultElement.style.color = "red";
+                } else {
+                    resultElement.innerText = "Incorrect. Try again.";
+                    resultElement.style.color = "red";
+                }
+            }
+        }
+        var correctAnswer28 = ["There are a lot of discussions about the impact of climate change.", "There are lots of discussions about the impact of climate change.", "There are many discussions about the impact of climate change." ];
+        var attempts28 = 0;
+
+        function checkAnswer28() {
+            var userAnswer = document.getElementById('user-answer28').value;
+            var resultElement = document.getElementById('result28');
+            if(correctAnswer28.includes(userAnswer)) {
+                resultElement.innerText = "Correct!";
+                resultElement.style.color = "green";
+            } else {
+                attempts28++;
+                if(attempts28 >= 3) {
+                    resultElement.innerText = "Incorrect. The correct answer is: " + correctAnswer28;
+                    resultElement.style.color = "red";
+                } else {
+                    resultElement.innerText = "Incorrect. Try again.";
+                    resultElement.style.color = "red";
+                }
+            }
+        }
